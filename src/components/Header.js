91ac740cd470379dab2938d7b03b4cef946472ce
orpheus_lim <twinstarwon@gmail.com>
2017-02-08 22:49:32 +0900

    17.02.08

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import './Header.css';
 
-const MenuItem = ({active, children, to}) => (
-    <div className="menu-item">
-            {children}
-    </div>
-)
+class Header extends React.Component {
+    render() {
+      const logoutButton = (
+          <li>
+              <a>
+                  <i className="material-icons">lock_open</i>
+              </a>
+          </li>
+      );
 
-const Header = () => {
-    return (
-            <div className="menu">
-                <MenuItem>온숨</MenuItem>
-            </div>
-    );
+              return (
+                  <nav>
+                      <div className="nav-wrapper black">
+                          <a className="brand-logo center">온숨</a>
+                          <div className="right">
+                              <ul>
+                                  { this.props.isLoggedIn ? logoutButton : null }
+                              </ul>
+                          </div>
+                      </div>
+                  </nav>
+              );
+          }
+        }
+Header.propTypes = {
+    isLoggedIn: React.PropTypes.bool,
+    onLogout: React.PropTypes.func
+};
+
+Header.defaultProps = {
+    isLoggedIn: false,
+    onLogout: () => { console.error("logout function not defined");}
 };
 
 export default Header;
