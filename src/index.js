6a9787087a19bce5b5f8536c1aea2805a2a88cfe
orpheus_lim <twinstarwon@gmail.com>
2017-02-08 11:35:02 +0900

    router stage 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './App';
+import Login from './router/Login';
+import Register from './router/Register';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+    <IndexRoute component={App}/>
+    <Route path="login" component={Login}/>
+    <Route path="Register" component={Register}/>
+  </Router>,
   document.getElementById('root')
 );
---
+++ src/router/Register.js
@@ -0,0 +1,12 @@
+import { React, Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return(
+      <div>
+        회원가입
+      </div>
+    );
+  }
+}
+export default Register;
