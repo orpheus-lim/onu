5364e3b2694d58ed591d8a78bd6bb93a5ad2f93c
orpheus_lim <twinstarwon@gmail.com>
2017-02-18 09:47:33 +0900

    register 미완료

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { App, Home, Login, Register } from './containers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
 
-import './index.css';
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-        <Route path="login" component={Login}/>
-        <Route path="register" component={Register}/>
-      </Route>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="login" component={Login}/>
+          <Route path="register" component={Register}/>
+        </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/authentication.js
@@ -0,0 +1,46 @@
+import * as types from '../actions/ActionTypes';
+import update from 'react-addons-update';
+
+const initialState = {
+    login: {
+        status: 'INIT'
+    },
+    register: {
+        status: 'INIT',
+        error: -1
+    },
+    status: {
+        isLoggedIn: false,
+        currentUser: '',
+    }
+};
+
+export default function authentication(state, action) {
+  if(typeof state === "undefined")
+    state = {initialState};
+    switch(action.type) {
+        /* LOGIN */
+        case types.AUTH_REGISTER:
+            return update(state, {
+                register: {
+                    status: { $set: 'WAITING' },
+                    error: { $set: -1 }
+                }
+            });
+        case types.AUTH_REGISTER_SUCCESS:
+            return update(state, {
+                register: {
+                    status: { $set: 'SUCCESS' }
+                }
+            });
+        case types.AUTH_REGISTER_FAILURE:
+            return update(state, {
+                register: {
+                    status: { $set: 'FAILURE' },
+                    error: { $set: action.error }
+                }
+            });
+        default:
+            return state;
+    }
+}
